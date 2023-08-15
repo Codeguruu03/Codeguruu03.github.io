a67cec0f8e8f711a0ce6b19fc5ad7ba9250c95d0
John Doe <johndoe@example.com>
2023-08-15 12:58:27 +0530

    gallery page created and make more attractive

--- src/javascript/gallery.js
+++ src/javascript/gallery.js
@@ -1,43 +1,21 @@
-const slider = document.querySelector('.slider');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const slideDescriptions = document.querySelectorAll('.slide-description');
-
-let slideIndex = 0;
-
-function showSlide(index) {
-    if (index >= slider.children.length) {
-        slideIndex = 0;
-    } else if (index < 0) {
-        slideIndex = slider.children.length - 1;
-    }
-
-    slider.style.transform = `translateX(-${slideIndex * 100}%)`;
-    updateSlideDescription();
-}
-
-function updateSlideDescription() {
-    slideDescriptions.forEach((description, index) => {
-        if (index === slideIndex) {
-            description.style.display = 'block';
-        } else {
-            description.style.display = 'none';
-        }
-    });
-}
-
-prevBtn.addEventListener('click', () => {
-    slideIndex--;
-    showSlide(slideIndex);
+"use strict";
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let btn = document.getElementById('btn');
+let rocks = document.getElementById('rocks');
+let forest = document.getElementById('forest');
+let water = document.getElementById('water');
+let header = document.getElementById('header');
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    text.style.top = 50 + value * -.1 + '%';
+    bird2.style.top = value * -1.5 + 'px';
+    bird2.style.left = value * 2 + 'px';
+    bird1.style.top = value * -1.5 + 'px';
+    bird1.style.left = value * -5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    rocks.style.top = value * -.12 + 'px';
+    forest.style.top = value * .25 + 'px';
+    header.style.top = value * .5 + 'px';
 });
-
-nextBtn.addEventListener('click', () => {
-    slideIndex++;
-    showSlide(slideIndex);
-});
-
-// Automatically change slide every 3 seconds
-setInterval(() => {
-    slideIndex++;
-    showSlide(slideIndex);
-}, 3000);
