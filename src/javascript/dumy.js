388c36e398079728e89c93128dbce173f7963fda
John Doe <johndoe@example.com>
2023-08-17 17:16:51 +0530

    changes

--- src/javascript/dumy.js
+++ src/javascript/dumy.js
@@ -1,20 +1,14 @@
-const videoPlayer = document.getElementById('videoPlayer');
-const fullscreenButton = document.getElementById('fullscreenButton');
+const slider = document.querySelector(".slider");
+let slideIndex = 0;
 
-fullscreenButton.addEventListener('click', toggleFullscreen);
-
-function toggleFullscreen() {
-    if (!document.fullscreenElement) {
-        videoPlayer.requestFullscreen().catch(err => {
-            console.log(`Error attempting to enable full-screen mode: ${err.message}`);
-        });
-    } else {
-        document.exitFullscreen();
+function slide() {
+    slideIndex++;
+    if (slideIndex > 2) {
+        slideIndex = 0;
     }
+    const translateValue = -slideIndex * 100;
+    slider.style.transform = `translateX(${translateValue}%)`;
 }
 
-videoPlayer.addEventListener('click', toggleVideoSize);
-
-function toggleVideoSize() {
-    videoPlayer.classList.toggle('full-screen');
-}
+// Change slide every 3 seconds
+setInterval(slide, 3000);
