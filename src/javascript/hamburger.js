63dcb0878e552377fc98c21062f74caa72f76b38
NerdyGamer611 <deepeshvcd6273@gmail.com>
2023-08-18 19:46:41 +0530

    added navigation bar for about us page

---
+++ src/javascript/hamburger.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const navigation = document.querySelector(".navigation");
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navigation.classList.toggle("active");
+    });
+});
