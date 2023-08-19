5b3512eb122aecbed07d66e53491c735b5a22724
John Doe <johndoe@example.com>
2023-08-19 22:47:37 +0530

    gallery page new created and new package page is created

--- src/javascript/gallery.js
+++ src/javascript/gallery.js
@@ -1,21 +1,52 @@
-"use strict";
-let text = document.getElementById('text');
-let bird1 = document.getElementById('bird1');
-let bird2 = document.getElementById('bird2');
-let btn = document.getElementById('btn');
-let rocks = document.getElementById('rocks');
-let forest = document.getElementById('forest');
-let water = document.getElementById('water');
-let header = document.getElementById('header');
-window.addEventListener('scroll', function () {
-    let value = window.scrollY;
-    text.style.top = 50 + value * -.1 + '%';
-    bird2.style.top = value * -1.5 + 'px';
-    bird2.style.left = value * 2 + 'px';
-    bird1.style.top = value * -1.5 + 'px';
-    bird1.style.left = value * -5 + 'px';
-    btn.style.marginTop = value * 1.5 + 'px';
-    rocks.style.top = value * -.12 + 'px';
-    forest.style.top = value * .25 + 'px';
-    header.style.top = value * .5 + 'px';
-});
+//selecting all required elements
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelectorAll(".gallery .image");
+
+window.onload = ()=>{ //after window loaded
+  filterItem.onclick = (selectedItem)=>{ //if user click on filterItem div
+    if(selectedItem.target.classList.contains("item")){ //if user selected item has .item class
+      filterItem.querySelector(".active").classList.remove("active"); //remove the active class which is in first item
+      selectedItem.target.classList.add("active"); //add that active class on user selected item
+      let filterName = selectedItem.target.getAttribute("data-name"); //getting data-name value of user selected item and store in a filtername variable
+      filterImg.forEach((image) => {
+        let filterImges = image.getAttribute("data-name"); //getting image data-name value
+        //if user selected item data-name value is equal to images data-name value
+        //or user selected item data-name value is equal to "all"
+        if((filterImges == filterName) || (filterName == "all")){
+          image.classList.remove("hide"); //first remove the hide class from the image
+          image.classList.add("show"); //add show class in image
+        }else{
+          image.classList.add("hide"); //add hide class in image
+          image.classList.remove("show"); //remove show class from the image
+        }
+      });
+    }
+  }
+  for (let i = 0; i < filterImg.length; i++) {
+    filterImg[i].setAttribute("onclick", "preview(this)"); //adding onclick attribute in all available images
+  }
+}
+
+//fullscreen image preview function
+//selecting all required elements
+const previewBox = document.querySelector(".preview-box"),
+categoryName = previewBox.querySelector(".title p"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+shadow = document.querySelector(".shadow");
+
+function preview(element){
+  //once user click on any image then remove the scroll bar of the body, so user cant scroll up or down
+  document.querySelector("body").style.overflow = "hidden";
+  let selectedPrevImg = element.querySelector("img").src; //getting user clicked image source link and stored in a variable
+  let selectedImgCategory = element.getAttribute("data-name"); //getting user clicked image data-name value
+  previewImg.src = selectedPrevImg; //passing the user clicked image source in preview image source
+  categoryName.textContent = selectedImgCategory; //passing user clicked data-name value in category name
+  previewBox.classList.add("show"); //show the preview image box
+  shadow.classList.add("show"); //show the light grey background
+  closeIcon.onclick = ()=>{ //if user click on close icon of preview box
+    previewBox.classList.remove("show"); //hide the preview box
+    shadow.classList.remove("show"); //hide the light grey background
+    document.querySelector("body").style.overflow = "auto"; //show the scroll bar on body
+  }
+}
