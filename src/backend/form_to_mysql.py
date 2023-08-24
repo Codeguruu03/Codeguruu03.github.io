d01d43b7f634594ed28c5b7f90c76fe77a4af2d5
KrishKoria <krishkoria2004@gmail.com>
2023-08-24 14:06:12 +0530

    fixed package form

--- src/backend/form_to_mysql.py
+++ src/backend/form_to_mysql.py
@@ -50,15 +50,15 @@
 #     app.run(debug=True)
 
 
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, redirect,flash
 import mysql.connector
-
+import time
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'NerdyGamer611',
+    'password': '',
     'database': 'travelbuddy'
 }
 
@@ -66,31 +66,32 @@ db_config = {
 def pkg_form():
     return render_template('form.html')
 
-@app.route('/stash', methods=['POST'])
+@app.route('/src/backend/stash', methods=['POST'])
 def submit():
     
-    full_name: request.form['full-name']
-    country_code: request.form['country-code']
-    phone_number: request.form['phone-number']
-    email: request.form['email']
-    nationality: request.form['nationality']
-    # num_tourists: request.form['num-tourists']
-    # package: request.form['package']
-    # month_of_travel: request.form['month-of-travel']
+    full_name = request.form['full-name']
+    country_code = request.form['country-code']
+    phone_number = request.form['phone-number']
+    mail = request.form['email']
+    nation = request.form['nationality']
+    tourists_num = request.form['num-tourists']
+    plan = request.form['package']
+    month_of_travel = request.form['month-of-travel']
     
     db_connection = mysql.connector.connect(**db_config)
     cursor = db_connection.cursor()
     print('connected')
     
-    query = "insert into travel_data (full_name, country_code, phone_number, email, nationality, num_tourists,package,month_of_travel) values (%s,%s,%s,%s,%s,%s,%s,%s)"
-    values = (full_name, country_code, phone_number,email,nationality)
+    query = "INSERT INTO travel_data (full_name, country_code, phone_number, email, nationality,num_tourists,package,month_of_travel) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
+    values = (full_name, country_code, phone_number,mail,nation,tourists_num,plan,month_of_travel)
     cursor.execute(query,values)
     
     db_connection.commit()
     cursor.close()
     db_connection.close()
     
-    return "Data Saved."
+    flash("Data Saved, redirecting you to home page...")
+    return redirect("https://deepesh611.github.io/deep-home.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port = 5550)
